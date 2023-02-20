bfa05d3638c1cd8fbcb95c91659e14cd92c640eb
bmsully <bsully@mit.edu>
2023-02-20 00:33:50 -0500

    removed moved files

--- spin.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-# File to "spin" processor, used to check if shell is hanging when using '&'
-
-with open("numbers.txt", "w") as f:
-    for i in range(10000000):
-        f.write("%d\n" % i)
-print("done writing")

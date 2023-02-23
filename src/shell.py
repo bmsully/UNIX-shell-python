e4c682523d6ff448eb8d0c16dc83cf09ccafef38
bmsully <bsully@mit.edu>
2023-02-23 17:55:02 -0500

    modified looping script to functional form

--- src/shell.py
+++ src/shell.py
@@ -2,25 +2,46 @@
 
 import os, sys
 
-# Loop forever
-while True:
-    user_input = input("$ ")
-    if user_input == "exit":
-        break
-    if user_input == "":
-        continue
+def loop():
+    while True:
+        user_input = input("$ ")
+        if user_input == "exit":
+            break
+        if user_input == "":
+            continue
+        if user_input == '|': #Augment with other operators
+            print("Invalid null command.")
+            continue
+        if user_input in ['>', '>>', '<', '<<']: #Augment this list with other operators
+            print("Missing name for redirect.")
+            continue
 
+        # Parse & Tokenize input
+        tokens = parse(user_input)
+
+        # Fork Processes
+        fork_processes(tokens)
+
+def parse(user_input):
     tokens = user_input.split()
     print("command", tokens[0])
     print("args", tokens)
+    return tokens
 
+def fork_processes(tokens):
     fork_id = os.fork()
-    if fork_id == 0: # we are the child
+    if fork_id == 0: # Child process
         os.execvp(tokens[0], tokens)
     if tokens[-1] != "&": # continue without waiting
         os.wait()
+    return
+
+if __name__ == "__main__":
+    loop()
 
 # How to differentiate between files and commands?
+
+# Reorganize into python functions
 
 """
 Todo List:

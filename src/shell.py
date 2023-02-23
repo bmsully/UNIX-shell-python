1d4fcc93920b0af0bafa0ec63baf48b5a34b2de0
bmsully <bsully@mit.edu>
2023-02-23 00:03:18 -0500

    printing for debugging, words.txt for output

--- src/shell.py
+++ src/shell.py
@@ -11,12 +11,16 @@ while True:
         continue
 
     tokens = user_input.split()
+    print("command", tokens[0])
+    print("args", tokens)
 
     fork_id = os.fork()
     if fork_id == 0: # we are the child
         os.execvp(tokens[0], tokens)
     if tokens[-1] != "&": # continue without waiting
         os.wait()
+
+# How to differentiate between files and commands?
 
 """
 Todo List:
